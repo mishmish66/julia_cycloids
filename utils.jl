a4e91681f7c430e802bb65f1d4dd00e50efe1354
Misha Lvovsky <m.lvovsky@protonmail.com>
2022-08-18 10:12:25 -0400

    added cross product to utils.jl

--- utils.jl
+++ utils.jl
@@ -12,3 +12,7 @@ const NorD = Union{Num,Float}
 function rotmat(θ::T) where T <: Number
     return [cos(θ) sin(θ); sin(θ) -cos(θ)]
 end
+
+function cross(v1::T, v2::T) where T <: Vector{T_invec} where T_invec <: Number
+    return v1[1] * v2[2] - v1[2] * v2[1]
+end
