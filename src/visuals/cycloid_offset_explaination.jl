633edc00cc5173c5a7075f4e08b25d8fa769bf19
Misha Lvovsky <m.lvovsky@protonmail.com>
2022-08-19 13:26:04 -0400

    added ratio change

--- src/visuals/cycloid_offset_explaination.jl
+++ src/visuals/cycloid_offset_explaination.jl
@@ -11,7 +11,8 @@ f = Figure(resolution=(1920, 1920))
 
 inset_slider = Slider(f[2, 1], range = -0.5:0.01:0.5, startvalue=0.0)
 eccentricity_slider = Slider(f[3, 1], range= -0.5:0.01:0.5, startvalue=0.15)
-step_slider = Slider(f[4, 1], range= -8π/100:8π/2000:8π/100, startvalue=0.15)
+rat_slider = Slider(f[4, 1], range = -10:1:10, startvalue=5)
+step_slider = Slider(f[5, 1], range= -8π/100:8π/2000:8π/100, startvalue=0.15)
 
 # Create the Cycloid ==============================================================================
 
@@ -22,10 +23,11 @@ l2 = 1.0
 edge = eccentricity[] + l2
 edge = edge * 1.5
 θ = Observable(0.0)
-pos = @lift(rotmat($θ)*[0.0, $eccentricity] + [0.0, l2])
-rot = @lift(-$θ/4)
+pos = @lift(rotmat($θ)*[$eccentricity, 0.0] + [l2, 0.0])
+rat = rat_slider.value
+rot = @lift(-$θ/($rat-1))
 
-cyc = @lift(Cycloid($eccentricity, l2, -1 + 1/5, $rot, [0, 0], $inset))
+cyc = @lift(Cycloid($eccentricity, l2, -1 + 1/$rat, $rot, [0, 0], $inset))
 
 circle = @lift(PCircle($pos, $inset, 0.0))
 
