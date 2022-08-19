82bbf84ac460dc31d4714a562c3d93173e94e375
Misha Lvovsky <m.lvovsky@protonmail.com>
2022-08-19 13:12:16 -0400

    finished up this explaination

--- src/visuals/cycloid_offset_explaination.jl
+++ src/visuals/cycloid_offset_explaination.jl
@@ -2,45 +2,56 @@ include("../utils.jl")
 include("../draw.jl")
 using GLMakie
 
-# Create the Axes =================================================================================
+# Create Figure ===================================================================================
 
 set_theme!(theme_black())
 f = Figure(resolution=(1920, 1920))
-top_left = GridLayout(f[1, 1])
-inside = GridLayout(top_left[1, 1])
-ax = Axis(f[1, 1])
-
-ax.aspect = AxisAspect(1)
 
 # Add Slider ======================================================================================
 
 inset_slider = Slider(f[2, 1], range = -0.5:0.01:0.5, startvalue=0.0)
+eccentricity_slider = Slider(f[3, 1], range= -0.5:0.01:0.5, startvalue=0.15)
+step_slider = Slider(f[4, 1], range= -8π/100:8π/2000:8π/100, startvalue=0.15)
 
-# Draw the Cycloid
+# Create the Cycloid ==============================================================================
 
 inset = inset_slider.value
 k = 5
-eccentricity = 0.1
+eccentricity = eccentricity_slider.value
 l2 = 1.0
-edge = eccentricity + l2
+edge = eccentricity[] + l2
 edge = edge * 1.5
+θ = Observable(0.0)
+pos = @lift(rotmat($θ)*[0.0, $eccentricity] + [0.0, l2])
+rot = @lift(-$θ/4)
+
+cyc = @lift(Cycloid($eccentricity, l2, -1 + 1/5, $rot, [0, 0], $inset))
+
+circle = @lift(PCircle($pos, $inset, 0.0))
+
+# Create the Axes =================================================================================
+
+ax = Axis(f[1, 1])
+ax.aspect = AxisAspect(1)
 limits!(-edge, edge, -edge, edge)
 
-θ = Observable(0.0)
-pos = @lift(rotmat($θ)*[eccentricity, 0.0])
+# Draw the Cycloid ================================================================================
 
-cyc = @lift(Cycloid(eccentricity, l2, -1 - 1/3, 0.0, $pos, $inset))
+lines!(circle, color=:cyan)
+lines!(cyc, color=:orange)
 
-lines!(cyc)
+# Animate! ========================================================================================
+
+animation_step = step_slider.value
 
 function update()
-    θ[] += 8π/250
+    θ[] += animation_step[]
     if θ[] >= 8π
         θ[] -= 8π
     end
 end
 
 while true
-    sleep(0.005)
+    sleep(0.01)
     update()
 end
